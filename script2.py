3c1ecf28b58da2b821ea1b0619e32c6ab7ce84c5
Cristobal <cridonoso@udec.cl>
2020-06-02 02:28:13 -0400

    script predictions

---
+++ script2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+import sys
+import subprocess
+import time
+
+dataset  = sys.argv[1]
+
+for unit_type in ['lstm', 'phased']:
+	for fold_n in [1,2]:
+	    start = time. time()
+	    command1 = 'python predict.py {} {} {}'.format(dataset, unit_type, fold_n)
+	    print('executing: ',command1)
+	    try:
+	        subprocess.call(command1, shell=True)
+	    except:
+	        print('ERROR IN: ',command1)
+	    end = time. time()
+	    print('{} fold_{} takes {}'.format(unit_type, fold_n,(end - start)))
