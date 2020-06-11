d9d60ec2e6c0221d9e541fe0824a23ac114192bc
Cristobal <cridonoso@udec.cl>
2020-06-11 03:23:37 -0400

    script

--- script.py
+++ script.py
@@ -3,17 +3,19 @@ import sys
 import subprocess
 import time
 
-dataset  = sys.argv[1]
-unit_type = sys.argv[2]
+# dataset = sus.argv[1]
+# unit_type = sys.argv[2]
 
-for normalization in ['n1', 'n2']:
-	for fold_n in [0,1,2]:
-	    start = time. time()
-	    command1 = 'python main.py {} {} {} {}'.format(dataset, fold_n, unit_type, normalization)
-	    print('executing: ',command1)
-	    try:
-	        subprocess.call(command1, shell=True)
-	    except:
-	        print('ERROR IN: ',command1)
-	    end = time. time()
-	    print('{} fold_{} takes {}'.format(unit_type, fold_n,(end - start)))
+for dataset in ['linear', 'wise', 'css']:
+	for unit_type in ['phased', 'lstm']:
+		for normalization in ['n2']:
+			for fold_n in [0,1,2]:
+			    start = time. time()
+			    command1 = 'python main.py {} {} {} {}'.format(dataset, fold_n, unit_type, normalization)
+			    print('executing: ',command1)
+			    try:
+			        subprocess.call(command1, shell=True)
+			    except:
+			        print('ERROR IN: ',command1)
+			    end = time. time()
+			    print('{} fold_{} takes {}'.format(unit_type, fold_n,(end - start)))
