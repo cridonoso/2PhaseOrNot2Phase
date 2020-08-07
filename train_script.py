4111289c5718b794607cd267e56a5fc7534b67fb
Cristobal <cridonoso@udec.cl>
2020-08-07 02:01:06 -0400

    n2 included in train_script

--- train_script.py
+++ train_script.py
@@ -11,28 +11,26 @@ except:
 	gpu = ''
 	print('Using -by default- GPU number 0')
 
-
-normalization = sys.argv[3]
-
-for unit_type in ['plstm', 'lstm']:
-	for fold_n in range(3):
-	    start = time. time()
-	    command1 = 'python main.py {} --dataset {} \
-	    						      --fold_n  {} \
-	    						      --rnn_unit {} \
-	    						      --normalization {}'.format(gpu,
-	    						   							     dataset, 
-	    													     fold_n, 
-	    													     unit_type, 
-	    													     normalization)
-	    print('::: TRAINING SCRIPT :::\n:::::::::::::::::::::::\n- dataset: {}\n- unit: {}\n- norm: {}\n- fold: {}\n:::::::::::::::::::::::'.format(dataset,
-	    																																			unit_type,
-	    																																 			normalization,
-	    																																 			fold_n),
-	    	  end='\r')
-	    try:
-	        subprocess.call(command1, shell=True)
-	    except Exception as e:
-	        print(e)
-	    end = time. time()
-	    print('{} fold_{} takes {}'.format(unit_type, fold_n,(end - start)))
+for normalization in ['n1', 'n2']:
+	for unit_type in ['plstm', 'lstm']:
+		for fold_n in range(3):
+			start = time. time()
+			command1 = 'python main.py {} --dataset {} \
+									      --fold_n  {} \
+									      --rnn_unit {} \
+									      --normalization {}'.format(gpu,
+									   							     dataset, 
+																     fold_n, 
+																     unit_type, 
+																     normalization)
+			print('::: TRAINING SCRIPT :::\n:::::::::::::::::::::::\n- dataset: {}\n- unit: {}\n- norm: {}\n- fold: {}\n:::::::::::::::::::::::'.format(dataset,
+																																						unit_type,
+																																			 			normalization,
+																																			 			fold_n),
+				  end='\r')
+			try:
+			    subprocess.call(command1, shell=True)
+			except Exception as e:
+			    print(e)
+			end = time. time()
+			print('{} fold_{} takes {}'.format(unit_type, fold_n,(end - start)))
